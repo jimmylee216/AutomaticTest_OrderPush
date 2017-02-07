13fbe27541ad30c5a0a9e1c386b5f8d9786d2efd
Jimmy Lee <jimmylee216@foxmail.com>
2017-02-07 16:58:19 +0800

    新项目提交

---
+++ mianshui365/src/main/java/hna/mianshui365/App.java
@@ -0,0 +1,13 @@
+package hna.mianshui365;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
