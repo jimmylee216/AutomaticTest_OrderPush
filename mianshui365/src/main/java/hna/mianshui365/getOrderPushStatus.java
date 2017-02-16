abff19263f55be22058c42e9d289ef71dcac6ee8
Jimmy Lee <jimmylee216@foxmail.com>
2017-02-16 17:33:08 +0800

    解决了testNG包多余的引用

--- mianshui365/src/main/java/hna/mianshui365/getOrderPushStatus.java
+++ mianshui365/src/main/java/hna/mianshui365/getOrderPushStatus.java
@@ -13,7 +13,6 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.testng.annotations.Test;
 import java.io.BufferedReader;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -25,6 +24,7 @@ import org.json.JSONObject;
  * @version 1.0 2017/2/9
  * @return 返回响应报文
  **/
+
 
 public class getOrderPushStatus {
 	private String url = null;
